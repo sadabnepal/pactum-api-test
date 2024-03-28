751207791990189ea9672e733f2f71fbd815340f
MD SADAB SAQIB <sadabnepal1993@gmail.com>
2024-03-28 23:05:00 +1100

    removed repetitive test

--- tests/specs/getUser.ts
+++ tests/specs/getUser.ts
@@ -1,5 +1,4 @@
 import { spec, expect } from 'pactum';
-import { like } from 'pactum-matchers';
 import { startServer, stopServer } from '../../mocks/helper/server';
 import { ENV } from '../env/manager';
 import { fetchMockedUserResponseDataByFileName } from '../../mocks/helper/fileHandler';
@@ -41,23 +40,6 @@ describe('fetch user test suite', () => {
     it('should get user 30', async () => {
         const data = await spec().get(`${ENV.BASE_URL}/api/users/30`).toss();
         expect(data.statusCode, 404);
-    });
-
-    it('should create user', async () => {
-        const payload = {
-            name: 'sadab',
-            job: 'tester'
-        };
-
-        await spec()
-            .post(`${ENV.BASE_URL}/api/users`)
-            .withJson(payload)
-            .expectStatus(201)
-            .expectJsonMatch({
-                ...payload,
-                id: like(''),
-                createdAt: like('')
-            });
     });
 
 });
